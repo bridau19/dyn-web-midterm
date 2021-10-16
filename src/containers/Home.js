4a8db458c59aa4bae5efdc907b4b0dc192476864
bridau19 <bridau19@gmail.com>
2021-10-15 21:20:23 -0400

    added API and updated links in README

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
+import axios from 'axios';
 
 
 function Home() {
+    const [position, setPosition] = useState([]);
+
+    const URL = `http://api.open-notify.org/iss-now.json`;
+    
+    
+    useEffect(() => {
+        if (position) {
+            axios
+            .get(URL)
+            .then(response => {
+                const lat = response.data.iss_position.latitude;
+                const lon = response.data.iss_position.longitude;
+                setPosition([lat, lon]);
+            })
+            .catch(function(error) {
+                console.warn(error);
+            });
+        }
+    }, [URL, position]);
+
+    // const positions = position.map(x => <p>{x}</p>);
+    console.log("position: ", position);
+
+    const lat = position[0];
+    const lon = position[1];
     return (
         <div>
-            <h1>Midterm Project</h1>
+            <h1>ISS Current Location</h1>
+            <p>{lat}, {lon}</p>
         </div>
     );
 }
